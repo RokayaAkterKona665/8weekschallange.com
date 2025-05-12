a42a75d64f1f197dc193021f51b5bb5a694a2baf
RokayaAkterKona665 <rokayakona67@gmail.com>
2025-05-12 20:15:50 +0600

    Add files via upload

---
+++ B. Customer Transactions.sql
@@ -0,0 +1,15 @@
+--1. What is the unique count and total amount for each transaction type?
+select 
+	distinct txn_type,
+	count(  txn_type) as unique_customer,
+	sum(txn_amount) as total_amount
+from customer_transactions
+group by txn_type
+order by unique_customer
+--2. What is the average total historical deposit counts and amounts for all customers?
+
+--3. For each month - how many Data Bank customers make more than 1 deposit and either 
+--1 purchase or 1 withdrawal in a single month?
+--4. What is the closing balance for each customer at the end of the month?
+--5. What is the percentage of customers who increase their closing balance by more 
+--than 5%?
